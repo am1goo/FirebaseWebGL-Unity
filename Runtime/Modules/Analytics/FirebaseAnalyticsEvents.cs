2f263e72706679198d73f980ad7c37b46009f5cd
Pavel Shestakov <0xShestakov@gmail.com>
2026-02-26 08:56:45 +0300

    added Firebase Analytics built-in event names

---
+++ Runtime/Modules/Analytics/FirebaseAnalyticsEvents.cs
@@ -0,0 +1,33 @@
+namespace FirebaseWebGL
+{
+    public static class FirebaseAnalyticsEvents
+    {
+        public const string eventAddPaymentInfo = "add_payment_info";
+        public const string eventAddShippingInfo = "add_shipping_info";
+        public const string eventAddToCart = "add_to_cart";
+        public const string eventAddToWishlist = "add_to_wishlist";
+        public const string eventBeginCheckout = "begin_checkout";
+        public const string eventCheckoutProgress = "checkout_progress";
+        public const string eventException = "exception";
+        public const string eventGenerateLead = "generate_lead";
+        public const string eventLogin = "login";
+        public const string eventPageView = "page_view";
+        public const string eventPubchase = "purchase";
+        public const string eventRefund = "refund";
+        public const string eventRemoveFromCart = "remove_from_cart";
+        public const string eventScreenView = "screen_view";
+        public const string eventSearch = "search";
+        public const string eventSelectContent = "select_content";
+        public const string eventSelectItem = "select_item";
+        public const string eventSelectPromotion = "select_promotion";
+        public const string eventSetCheckoutOption = "set_checkout_option";
+        public const string eventShare = "share";
+        public const string eventSignUp = "sign_up";
+        public const string eventTimingComplete = "timing_complete";
+        public const string eventViewCart = "view_cart";
+        public const string eventViewItem = "view_item";
+        public const string eventViewItemList = "view_item_list";
+        public const string eventViewPromotion = "view_promotion";
+        public const string eventViewSearchResults = "view_search_results";
+    }
+}
